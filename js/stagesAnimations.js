ac1a2bc175e51d27806165d1110f34bc5eeac288
Worior-DEV <105605998+Worior-DEV@users.noreply.github.com>
2024-02-11 19:32:18 +0100

    test

--- js/stagesAnimations.js
+++ js/stagesAnimations.js
@@ -2,35 +2,49 @@ $(document).ready(function() {
     var scrolled = false;
 
     $(window).scroll(function() {
-        if (!scrolled) {
-            var windowHeight = $(window).height();
-            var scrollTop = $(window).scrollTop();
-            var elementOffset = $('#mainStage1').offset().top;
+        var windowHeight = $(window).height();
+        var scrollTop = $(window).scrollTop();
+        var elementOffset = $('#mainStage1').offset().top;
+        var elementHeight = $('#mainStage1').outerHeight();
 
-            if (scrollTop + windowHeight >= elementOffset) {
-                $('#Stage1').addClass('visible');
-                scrolled = true;
-            }
+        // Sprawdź czy div jest w całości widoczny na ekranie
+        var isFullyVisible = (elementOffset + elementHeight) <= (scrollTop + windowHeight) && elementOffset >= scrollTop;
+
+        if (!scrolled && isFullyVisible) {
+            $('#Stage1').addClass('visible');
+            scrolled = true;
+        } else if (!isFullyVisible) {
+            // Jeśli div nie jest widoczny, usuń klasę 'visible'
+            $('#Stage1').removeClass('visible');
+            scrolled = false;
         }
     });
 });
+
 
 $(document).ready(function() {
     var scrolled = false;
 
     $(window).scroll(function() {
-        if (!scrolled) {
-            var windowHeight = $(window).height();
-            var scrollTop = $(window).scrollTop();
-            var elementOffset = $('#mainStage2').offset().top;
+        var windowHeight = $(window).height();
+        var scrollTop = $(window).scrollTop();
+        var elementOffset = $('#mainStage2').offset().top;
+        var elementHeight = $('#mainStage2').outerHeight();
 
-            if (scrollTop + windowHeight >= elementOffset) {
-                $('#Stage2').addClass('visible');
-                scrolled = true;
-            }
+        // Sprawdź czy div jest w całości widoczny na ekranie
+        var isFullyVisible = (elementOffset + elementHeight) <= (scrollTop + windowHeight) && elementOffset >= scrollTop;
+
+        if (!scrolled && isFullyVisible) {
+            $('#Stage2').addClass('visible');
+            scrolled = true;
+        } else if (!isFullyVisible) {
+            // Jeśli div nie jest widoczny, usuń klasę 'visible'
+            $('#Stage2').removeClass('visible');
+            scrolled = false;
         }
     });
 });
+
 
 document.addEventListener('DOMContentLoaded', function() {
     var myDiv = document.getElementById('Stage3');
@@ -42,43 +56,61 @@ document.addEventListener('DOMContentLoaded', function() {
         // Jeśli górna krawędź diva jest widoczna na ekranie, dodaj klasę 'visible'
         if (divPosition < screenHeight) {
             myDiv.classList.add('visible');
+        } else {
+            // Jeśli div jest poza widokiem, usuń klasę 'visible'
+            myDiv.classList.remove('visible');
         }
     });
 });
+
 
 $(document).ready(function() {
     var scrolled = false;
 
     $(window).scroll(function() {
-        if (!scrolled) {
-            var windowHeight = $(window).height();
-            var scrollTop = $(window).scrollTop();
-            var elementOffset = $('#Stage4').offset().top;
+        var windowHeight = $(window).height();
+        var scrollTop = $(window).scrollTop();
+        var elementOffset = $('#Stage4').offset().top;
+        var elementHeight = $('#Stage4').outerHeight();
 
-            if (scrollTop + windowHeight >= elementOffset) {
-                $('#Stage4-2').addClass('visible');
-                scrolled = true;
-            }
+        // Sprawdź czy div jest w całości widoczny na ekranie
+        var isFullyVisible = (elementOffset + elementHeight) <= (scrollTop + windowHeight) && elementOffset >= scrollTop;
+
+        if (!scrolled && isFullyVisible) {
+            $('#Stage4-2').addClass('visible');
+            scrolled = true;
+        } else if (!isFullyVisible) {
+            // Jeśli div nie jest widoczny, usuń klasę 'visible'
+            $('#Stage4-2').removeClass('visible');
+            scrolled = false;
         }
     });
 });
+
 
 $(document).ready(function() {
     var scrolled = false;
 
     $(window).scroll(function() {
-        if (!scrolled) {
-            var windowHeight = $(window).height();
-            var scrollTop = $(window).scrollTop();
-            var elementOffset = $('#mainStage5').offset().top;
+        var windowHeight = $(window).height();
+        var scrollTop = $(window).scrollTop();
+        var elementOffset = $('#mainStage5').offset().top;
+        var elementHeight = $('#mainStage5').outerHeight();
 
-            if (scrollTop + windowHeight >= elementOffset) {
-                $('#Stage5').addClass('visible');
-                scrolled = true;
-            }
+        // Sprawdź czy div jest w całości widoczny na ekranie
+        var isFullyVisible = (elementOffset + elementHeight) <= (scrollTop + windowHeight) && elementOffset >= scrollTop;
+
+        if (!scrolled && isFullyVisible) {
+            $('#Stage5').addClass('visible');
+            scrolled = true;
+        } else if (!isFullyVisible) {
+            // Jeśli div nie jest widoczny, usuń klasę 'visible'
+            $('#Stage5').removeClass('visible');
+            scrolled = false;
         }
     });
 });
+
 
 document.addEventListener('DOMContentLoaded', function() {
     var myDiv = document.getElementById('StageJoinIn');
@@ -90,6 +122,9 @@ document.addEventListener('DOMContentLoaded', function() {
         // Jeśli górna krawędź diva jest widoczna na ekranie, dodaj klasę 'visible'
         if (divPosition < screenHeight) {
             myDiv.classList.add('visible');
+        } else {
+            // Jeśli div jest poza widokiem, usuń klasę 'visible'
+            myDiv.classList.remove('visible');
         }
     });
 });
